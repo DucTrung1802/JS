8e7953271dd1290b194721311df40a7c82ad5c16
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-05-09 18:04:06 +0700

    #11 before lecture 314

---
+++ Udemy/events-01-starting-setup/assets/scripts/events.js
@@ -0,0 +1,40 @@
+const button = document.querySelector("button");
+
+// button.onclick = function () {
+
+// };
+
+const buttonClickHandler = (event) => {
+  event.target.disabled = true;
+  console.log(event);
+};
+
+// button.addEventListener("click", buttonClickHandler);
+
+// setTimeout(() => {
+//   button.removeEventListener("click", buttonClickHandler);
+// }, 2000);
+
+// buttons.forEach((btn) => {
+//   btn.addEventListener("mouseenter", buttonClickHandler);
+// });
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+});
+
+const div = document.querySelector("div");
+
+div.addEventListener("mouseenter", (event) => {
+  console.log("CLICKED DIV");
+  console.log(event);
+});
+
+button.addEventListener("mouseenter", (event) => {
+  event.stopPropagation();
+  console.log("CLICKED BUTTON");
+  console.log(event);
+});
