ca337cababd0b566dbfb576ce23ebf31ea464340
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-05-12 17:04:07 +0700

    #21 before lecture 344

---
+++ Udemy/nums-strings-01-starting-setup/app.js
@@ -0,0 +1,24 @@
+function randomIntBetween(min, max) {
+  // min: 5, max: 10
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// console.log(randomIntBetween(1, 10));
+
+function productDescription(strings, productName, productPrice) {
+  console.log(strings);
+  console.log(productName);
+  console.log(productPrice);
+  let priceCategory = "cheap";
+  if (productPrice > 20) {
+    priceCategory = "fair";
+  }
+  // return `${strings[0]}${productName}${strings[1]}${priceCategory}${strings[2]}`;
+  return {name: productName, price: prodPrice}
+}
+
+const prodName = "JS Course";
+const prodPrice = 29.99;
+
+const productOutput = productDescription`This product (${prodName}) is ${prodPrice}.`;
+console.log(productOutput);
