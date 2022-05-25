11d8cee341ccf147df99ce021f904df5b8bb962f
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-05-25 16:03:43 +0700

    #31 after lecture 372

--- Udemy/http-01-starting-setup/assets/scripts/app.js
+++ Udemy/http-01-starting-setup/assets/scripts/app.js
@@ -1,20 +1,37 @@
-const xhr = new XMLHttpRequest();
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
 
-xhr.open("GET", "https://jsonplaceholder.typicode.com/posts");
+function sendHttpRequest(method, url) {
+  const promise = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-xhr.responseType = "json";
+    xhr.open(method, url);
 
-xhr.onload = function () {
-  // const listOfPosts = JSON.parse(xhr.response);
-  const listOfPosts = xhr.response;
+    xhr.responseType = "json";
+
+    xhr.onload = function () {
+      resolve(xhr.response);
+      // const listOfPosts = JSON.parse(xhr.response);
+    };
+
+    xhr.send();
+  });
+
+  return promise;
+}
+
+async function fetchPosts() {
+  const responseData = await sendHttpRequest(
+    "GET",
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+  const listOfPosts = responseData;
   for (const post of listOfPosts) {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
     listElement.append(postEl);
   }
-};
+}
 
-xhr.send();
+fetchPosts();
