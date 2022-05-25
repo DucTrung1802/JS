53b10aaa94ad60c80653fa4c8232fd7f24769d62
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-05-25 17:01:45 +0700

    #33 after lecture 378

--- Udemy/http-01-starting-setup/assets/scripts/app.js
+++ Udemy/http-01-starting-setup/assets/scripts/app.js
@@ -5,25 +5,34 @@ const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
 function sendHttpRequest(method, url, data) {
-  const promise = new Promise((resolve, reject) => {
-    const xhr = new XMLHttpRequest();
+  // const promise = new Promise((resolve, reject) => {
+  // const xhr = new XMLHttpRequest();
+  // xhr.open(method, url);
+  // xhr.responseType = "json";
+  // xhr.onload = function () {
+  //   if (xhr.status >= 200 && xhr.status < 300) {
+  //     resolve(xhr.response);
+  //   } else {
+  //     reject(new Error("Something went wrong!"));
+  //   }
+  //   // const listOfPosts = JSON.parse(xhr.response);
+  // };
+  // xhr.onerror = function () {
+  //   reject(new Error("Failed to send request!"));
+  // };
+  // xhr.send(JSON.stringify(data));
 
-    xhr.open(method, url);
+  // });
 
-    xhr.responseType = "json";
+  // return promise;
 
-    xhr.onload = function () {
-      resolve(xhr.response);
-      // const listOfPosts = JSON.parse(xhr.response);
-    };
-
-    xhr.send(JSON.stringify(data));
+  return fetch(url).then((response) => {
+    return response.json();
   });
-
-  return promise;
 }
 
 async function fetchPosts() {
+  // try {
   const responseData = await sendHttpRequest(
     "GET",
     "https://jsonplaceholder.typicode.com/posts"
@@ -36,6 +45,9 @@ async function fetchPosts() {
     postEl.querySelector("li").id = post.id;
     listElement.append(postEl);
   }
+  // } catch (error) {
+  //   alert(error.message);
+  // }
 }
 
 async function createPost(title, content) {
