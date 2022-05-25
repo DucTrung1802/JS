999295c3587d5f584efaafacf67e54fbd197942d
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-05-25 17:33:09 +0700

    #34 after lecture 381

--- Udemy/http-01-starting-setup/assets/scripts/app.js
+++ Udemy/http-01-starting-setup/assets/scripts/app.js
@@ -7,6 +7,7 @@ const postList = document.querySelector("ul");
 function sendHttpRequest(method, url, data) {
   // const promise = new Promise((resolve, reject) => {
   // const xhr = new XMLHttpRequest();
+  // xhr.setRequestHeader("Content-Type", "application/json");
   // xhr.open(method, url);
   // xhr.responseType = "json";
   // xhr.onload = function () {
@@ -26,28 +27,46 @@ function sendHttpRequest(method, url, data) {
 
   // return promise;
 
-  return fetch(url).then((response) => {
-    return response.json();
-  });
+  return fetch(url, {
+    method: method,
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.status >= 200 && response.status < 300) {
+        return response.json();
+      } else {
+        return response.json().then((errData) => {
+          console.log(errData);
+          throw new Error("Something went wrong - server-side.");
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      throw new Error("Something went wrong!");
+    });
 }
 
 async function fetchPosts() {
-  // try {
-  const responseData = await sendHttpRequest(
-    "GET",
-    "https://jsonplaceholder.typicode.com/posts"
-  );
-  const listOfPosts = responseData;
-  for (const post of listOfPosts) {
-    const postEl = document.importNode(postTemplate.content, true);
-    postEl.querySelector("h2").textContent = post.title.toUpperCase();
-    postEl.querySelector("p").textContent = post.body;
-    postEl.querySelector("li").id = post.id;
-    listElement.append(postEl);
+  try {
+    const responseData = await sendHttpRequest(
+      "GET",
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const listOfPosts = responseData;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
+    }
+  } catch (error) {
+    alert(error.message);
   }
-  // } catch (error) {
-  //   alert(error.message);
-  // }
 }
 
 async function createPost(title, content) {
