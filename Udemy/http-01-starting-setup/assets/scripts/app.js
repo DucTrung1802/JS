748d88a029e15abb81b41ea1498b8f3b7f71dbaf
Ly Duc Trung <trung.lyduc18@gmail.com>
2022-05-25 16:48:48 +0700

    #32 after lecture 376

--- Udemy/http-01-starting-setup/assets/scripts/app.js
+++ Udemy/http-01-starting-setup/assets/scripts/app.js
@@ -1,7 +1,10 @@
 const listElement = document.querySelector(".posts");
 const postTemplate = document.getElementById("single-post");
+const form = document.querySelector("#new-post form");
+const fetchButton = document.querySelector("#available-posts button");
+const postList = document.querySelector("ul");
 
-function sendHttpRequest(method, url) {
+function sendHttpRequest(method, url, data) {
   const promise = new Promise((resolve, reject) => {
     const xhr = new XMLHttpRequest();
 
@@ -14,7 +17,7 @@ function sendHttpRequest(method, url) {
       // const listOfPosts = JSON.parse(xhr.response);
     };
 
-    xhr.send();
+    xhr.send(JSON.stringify(data));
   });
 
   return promise;
@@ -30,8 +33,37 @@ async function fetchPosts() {
     const postEl = document.importNode(postTemplate.content, true);
     postEl.querySelector("h2").textContent = post.title.toUpperCase();
     postEl.querySelector("p").textContent = post.body;
+    postEl.querySelector("li").id = post.id;
     listElement.append(postEl);
   }
 }
 
-fetchPosts();
+async function createPost(title, content) {
+  const userId = Math.random();
+  const post = {
+    title: title,
+    body: content,
+    userId: userId,
+  };
+
+  sendHttpRequest("POST", "https://jsonplaceholder.typicode.com/posts", post);
+}
+
+fetchButton.addEventListener("click", fetchPosts);
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const enteredTitle = event.currentTarget.querySelector("#title").value;
+  const enteredContent = event.currentTarget.querySelector("#content").value;
+
+  createPost(enteredTitle, enteredContent);
+});
+
+postList.addEventListener("click", (event) => {
+  if (event.target.tagName === "BUTTON") {
+    const postId = event.target.closest("li").id;
+    sendHttpRequest(
+      "DELETE",
+      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    );
+  }
+});
