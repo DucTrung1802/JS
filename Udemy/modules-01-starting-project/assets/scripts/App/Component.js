356a25b1838237cf06ed2a59c872e577be5d442a
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-05-27 17:26:50 +0700

    #42 working on - lecture 414, 4:40

--- Udemy/modules-01-starting-project/assets/scripts/App/Component.js
+++ Udemy/modules-01-starting-project/assets/scripts/App/Component.js
@@ -1,24 +1,30 @@
-export class Component {
-    constructor(hostElementId, insertBefore = false) {
-      if (hostElementId) {
-        this.hostElement = document.getElementById(hostElementId);
-      } else {
-        this.hostElement = document.body;
-      }
-      this.insertBefore = insertBefore;
+/* eslint-disable brace-style */
+/* eslint-disable space-before-function-paren */
+/* eslint-disable linebreak-style */
+export default class Component {
+  constructor(hostElementId, insertBefore = false) {
+    if (hostElementId)
+    {
+      this.hostElement = document.getElementById(hostElementId)
+    } else
+    {
+      this.hostElement = document.body
     }
-  
-    detach() {
-      if (this.element) {
-        this.element.remove();
-        // this.element.parentElement.removeChild(this.element);
-      }
-    }
-  
-    attach() {
-      this.hostElement.insertAdjacentElement(
-        this.insertBefore ? 'afterbegin' : 'beforeend',
-        this.element
-      );
+    this.insertBefore = insertBefore
+  }
+
+  detach() {
+    if (this.element)
+    {
+      this.element.remove()
+      // this.element.parentElement.removeChild(this.element);
     }
   }
+
+  attach() {
+    this.hostElement.insertAdjacentElement(
+      this.insertBefore ? 'afterbegin' : 'beforeend',
+      this.element
+    )
+  }
+}
