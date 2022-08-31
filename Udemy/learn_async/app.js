58a56e0437955e52d5b61545cce36f47dbadf326
Ly Duc Trung <lyductrungmoney@gmail.com>
2022-08-31 09:33:19 +0700

    complete learning async

--- Udemy/learn_async/app.js
+++ Udemy/learn_async/app.js
@@ -1,51 +1,67 @@
-const button = document.querySelector("button");
-const output = document.querySelector("p");
+const button = document.querySelector('button');
+const output = document.querySelector('p');
 
 const getPosition = (opts) => {
-  const promise = new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(
-      (success) => {
-        resolve(success);
-      },
-      (error) => {
-        reject(error);
-        console.log(error);
-      },
-      opts
-    );
-  });
-  return promise;
+    const promise = new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+            (success) => {
+                resolve(success);
+            },
+            (error) => {
+                reject(error);
+            },
+            opts,
+        );
+    });
+    return promise;
 };
 
 const setTimer = (duration) => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Done!");
-    }, duration);
-  });
-  return promise;
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, duration);
+    });
+    return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-    .then((posData) => {
-      positionData = posData;
-      return setTimer(2000);
-    })
-    .catch((error) => {
-      console.log(error);
-    })
-    .then((data) => {
-      console.log(data, positionData);
-    });
-  setTimer(1000).then(() => {
-    console.log("Timer done!");
-  });
-  console.log("Getting position...");
+async function trackUserHandler() {
+    // console.log('hiiii');
+    // let posData;
+    // let timerData;
+    // try {
+    //     posData = await getPosition();
+    //     timerData = await setTimer(2000);
+    //     console.log(timerData, posData);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+    // console.log('Hello');
+    // .then((posData) => {
+    //     positionData = posData;
+    //     return setTimer(2000);
+    // })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     })
+    //     .then((data) => {
+    //         console.log(data, positionData);
+    //     });
+    // setTimer(1000).then(() => {
+    //     console.log('Timer done!');
+    // });
+    // console.log('Getting position...');
 }
 
-button.addEventListener("click", trackUserHandler);
+Promise.all([getPosition(), setTimer(1000)]).then((data) => {
+    console.log(data);
+});
+
+// Promise.allSettled([getPosition(), setTimer(1000)]).then((data) => {
+//     console.log(data);
+// });
+
+button.addEventListener('click', trackUserHandler);
 
 // let result = 0;
 
